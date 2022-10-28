7736ed52972ef9b48a58826598afa89b95f44bed
Patrick Harris <pdharris@udel.edu>
2022-10-27 22:57:30 -0400

    Got rid of warning message

--- src/quizzer/QuizList.tsx
+++ src/quizzer/QuizList.tsx
@@ -56,6 +56,9 @@ export const QuizList = ({
                         ></QuizView>
                     );
                 }
+                else {
+                    return (<div></div>)
+                }
             })}
         </div>
     );
